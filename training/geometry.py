d6333dd4c17d7dbc9f839bf3b1cdfcf7f9f31731
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-09 18:08:41 +0300

    Лямбда выражения.

--- training/geometry.py
+++ training/geometry.py
@@ -2,7 +2,10 @@ __author__ = 'user'
 
 from geom2d.point import *
 
-l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
 
-l2 = sorted(l1)
-print("ok")
+
+l2 = sorted(l1, cmp=lambda p1, p2: cmp=(p1.y, p2.y))
+
+print(l1)
+print(l2)
