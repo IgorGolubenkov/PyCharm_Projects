9e1dc9ae202d002d896b16cc47ffafb52797f4f3
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-10 17:46:01 +0300

    Преоброзование и фильтрация списков.

--- training/geometry.py
+++ training/geometry.py
@@ -2,13 +2,9 @@ __author__ = 'user'
 
 from geom2d.point import Point
 
-l = [Point(i, i*i) for i in range(-5, 6)]
+l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
 
-l2 = [ ]
-
-for el in l:
-    l2.append(Point(el.x, -el.y))
-
+l2 = list(filter(lambda p: p.x % 2 == 0, l))
 
 print(l)
 print(l2)
